54d1319be0e64e4cad27d9f4549f8ef00ffae92d
andrew-blankenship <andrew.blankenship@students.fhu.edu>
2025-04-29 12:29:39 -0500

    Build 2

--- main.py
+++ main.py
@@ -1,90 +1,69 @@
 #Rock, Paper, Scissors, Lizard, Spock
+import random
+
 winner = {'Rock': ['Lizard', 'Scissors'], 'Paper': ['Rock', 'Spock'], 'Scissors': ['Paper', 'Lizard'], 'Lizard': ['Spock', 'Paper'], 'Spock': ['Scissors', 'Rock']}
 options = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
 
+print('Welcome to Rock, Paper, Scissors, Lizard, Spock!\n First to 3 points wins!\n To start the game please type in one of the following choices Rock, Paper, Scissors, Lizard, or Spock')
+
+
 
 class Player():
-    def __init__(self, choice):
-        self.choice = choice
+    def __init__(self):
+        self.choice = 'none'
         self.wins = 0
         self.losses = 0
 
     def get_choice(self):
+        print('Please make your selection:')
         self.choice = input()
         while self.choice not in options:
             print('invalid choice, try again:')
             self.choice = input()
     
-    def get_choice(self):
-        self.choice = input()
-        while self.choice not in options:
-            print('invalid choice, try again:')
-            self.choice = input()
     
-    def winners(self, other):
-        if other.choice in winner[self.choice]:
-            print('User Wins!')
-            self.wins +=1
-            return 'User'
-        elif self.choice in winner[other.choice]:
-            print('Com Wins!')
-            other.wins +=1
-            return 'Com'
-        else:
-            print("It's a Draw.")
+    
 
 class Game():
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
         
-    '''def winners(self):
-        if self.player2 in winner[self.player1]:
+    
+    def winners(self):
+        if self.player2.choice in winner[self.player1.choice]:
             print('User Wins!')
+            self.player1.wins +=1
             return 'User'
-        elif self.player1 in winner[self.player2]:
+        elif self.player1.choice in winner[self.player2.choice]:
             print('Com Wins!')
+            self.player2.wins +=1
             return 'Com'
         else:
-            print("It's a Draw.")'''
+            print("It's a Draw.")
 
-    '''def get_choice(self):
-        self.choice = input()
-        while self.choice not in options:
-            print('invalid choice, try again:')
-            self.choice = input()'''
+    def game_loop(self):
+        while self.player1.wins < 3 and self.player2.wins < 3:
+            self.player1.get_choice()
+            self.player2.choice = random.choice(options)
+            self.winners()
 
-    '''def game_loop(self):
-        user = Player(self.player1)
-        computer = Player(self.player2)
-        while user.player.wins < 3 and computer.player.wins < 3:
-            if self.winners(self.player2.choice) == 'User':
-                user.wins += 1
-            choice = input()'''
+        if self.player1.wins == 3:
+            print('User is the Winner!')
+        else:
+            print('Com is the Winner!')
+        print(f'Final score:\n User: {self.player1.wins} Com: {self.player2.wins}')
             
         
 
 
-import random
-com_choice = random.choice(options)
-
-choice = input()
-while choice not in options:
-    print('invalid choice, try again:')
-    choice = input()
 
 
-user = Player(choice)
-computer = Player(com_choice)
+user = Player()
+computer = Player()
 
 
-while user.wins < 3 and computer.wins < 3:
-    user.winners(computer)
-    user.get_choice()
-    computer.choice = random.choice(options)
 
-if user.wins == 3:
-    print('User is the Winner!')
-else:
-    print('Com is the Winner!')
-print(user.wins, computer.wins)
+
+game = Game(user, computer)
+game.game_loop()
